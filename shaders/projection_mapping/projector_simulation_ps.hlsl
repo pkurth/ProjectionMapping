2ffd8fde84a6c688864ee4c8617c6c6eec3968af
Philipp Kurth <philipp.kurth@fau.de>
2022-05-13 08:51:29 +0200

    Removed black level from simulation for now

--- shaders/projection_mapping/projector_simulation_ps.hlsl
+++ shaders/projection_mapping/projector_simulation_ps.hlsl
@@ -64,7 +64,7 @@ ps_output main(ps_input IN)
 			float3 renderResult = renderResults[projIndex].SampleLevel(borderSampler, uv, 0).rgb; // Software intensity is baked into this already.
 			renderResult = sRGBToLinear(renderResult);
 
-			renderResult += (float3)0.01f; // Black level.
+			//renderResult += (float3)0.01f; // Black level.
 
 			color += renderResult * physicalAttenuation;
 		}
